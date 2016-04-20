1bd682cbd19b169b5f7c39c3b0da256439aa4f60
Taiiwo Llort <taiiwo@llort.gq>
2016-04-19 17:08:52 -0700

    cosmetic code changes

--- admin.py
+++ admin.py
@@ -4,52 +4,63 @@ from lib import util
 
 util = util.Util()
 
-if sys.argv[1] == "admin":
-    if len(sys.argv) > 2:
-        command = sys.argv[2]
-        if command == "add":
-            if len(sys.argv) < 4:
-                quit(
-                    "[E] Requires one ARG: The username of the user you want to\
-                    be an admin"
-                )
-            users = util.get_collection('users', db=util.config['auth_db'])
-            # find user
-            user = users.find_one({'user': sys.argv[3]})
-            if not user:
-                quit("[E] User does not exist")
-            util.update_user(user['_id'], {"$set": {'is_admin': True}})
-            quit("[-] User given admin privs")
+command = sub_command = False
+arg = []
+if len(sys.argv) > 1:
+    # main command
+    command = sys.argv[1]
+if len(sys.argv) > 2:
+    # command for the above command
+    sub_command = sys.argv[2]
+if len(sys.argv) > 3:
+    # arguments to sub_command
+    arg = sys.argv[3:]
 
-if sys.argv[1] == "datachest":
-    if len(sys.argv) > 2:
-        command = sys.argv[2]
+if not command:
+    # Maybe print help here
+    quit('[E] Requires atleast one command')
 
+if command == "admin":
+    if sub_command and sub_command == "add":
+        if len(arg) < 1:
+            quit(
+                "[E] Requires one ARG: The username of the user you want to\
+                be an admin"
+            )
+        users = util.get_collection('users', db=util.config['auth_db'])
+        # find user
+        user = users.find_one({'user': arg[0]})
+        if not user:
+            quit("[E] User does not exist")
+        util.update_user(user['_id'], {"$set": {'is_admin': True}})
+        quit("[-] User given admin privs")
+
+if command == "datachest":
         # Creates a new datachest
-        if command == 'create':
-            if len(sys.argv) < 4:
+        if sub_command and sub_command == 'create':
+            if len(arg) < 1:
                 quit("[E] Requires one argument: datachest name")
             # check if chest with that name exists
             users = util.get_collection('users', db=util.config['auth_db'])
-            user_exists = users.find({'user': sys.argv[3]}).count() > 0
+            user_exists = users.find({'user': arg[0]}).count() > 0
             if user_exists:
                 quit('[E] User exists')
             public = False
-            if len(sys.argv) > 4:
-                if sys.argv[4] == "public":
+            if len(arg) > 1:
+                if arg[1] == "public":
                     public = True
-            session = util.new_datachest(sys.argv[3], public=public)
+            session = util.new_datachest(arg[0], public=public)
             print("[-] Datachest created with session: %s" % session)
 
         # Adds a user to a datachest
-        if command == 'invite':
-            if len(sys.argv) < 5:
+        if sub_command and sub_command == 'invite':
+            if len(arg) < 2:
                 quit(
                     '''[E] Requires two arguments: Username of user to add, then
                     the name of the datachest'''
                 )
-            username = sys.argv[3]
-            datachest = sys.argv[4]
+            username = arg[0]
+            datachest = arg[1]
             # get user id
             user = util.get_collection(
                 'users', db=util.config['auth_db']
