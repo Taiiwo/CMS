f386f03f7ed1d3ecda00f75bf73995dc32d3432f
Emati Mitame <GitCommits@mita.me>
2016-04-23 14:37:26 +0100

    Refactor the fuck out of the api code

--- taiicms/api/__init__.py
+++ taiicms/api/__init__.py
@@ -1,5 +1,48 @@
-from .. import app
+from flask import make_response, jsonify
+from flask.ext.cors import CORS
 
+from .errors import error_names
+from .. import app, util, config
+
+CORS(app)  # make app work across origins
+util = util.Util(config["mongo"])
+
+
+def make_success_response(extra={}):
+    data_res = extra
+    data_res["success"] = True
+    res = make_response(jsonify(data_res), 200)
+    return res
+
+
+def make_error(error_name, extra_detail=None):
+    if isinstance(error_name, list):
+        res_errors = []
+        for err in error_name:
+            res_errors.append(errors.error_names[err])
+    else:
+        res_errors = [errors.error_names[error_name]]
+
+    res = {
+        "success": False,
+        "errors": res_errors,
+    }
+
+    if extra_detail:
+        res["extra"] = extra_detail
+
+    return res
+
+
+def make_error_response(*args, **kwargs):
+    error_res = make_error(*args, **kwargs)
+    res = make_response(jsonify(error_res), 400)
+    return res
+
+
+# has to go at the bottom to make sure functions are defined before we use them
 from . import (
-    user
+    user,
+    errors,
+    admin
 )
