dd2b683b3202d7de59a3e01c3f9437cf68217876
Emati Mitame <GitCommits@mita.me>
2016-05-08 15:23:22 +0100

    tab-to-spaces in main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from taiicms import app, socketio, config
 
 if __name__ == "__main__":
-	socketio.run(app, config["bind_addr"], config["port"], debug=config["debug"])
+    socketio.run(app, config["bind_addr"], config["port"], debug=config["debug"])
