609c05c284a56b38936598d1cef38db1cefd7b3b
Taiiwo Llort <taiiwo@llort.gq>
2016-05-18 20:20:26 +0100

    Complete NMI payment gateway wrapper

--- plugins/payment/payment.py
+++ plugins/payment/payment.py
@@ -6,23 +6,13 @@ import requests
 import re
 import xmltodict
 
-from flask import url_for, request
-
 class Payment():
-    redirect_url = "/"
-    def __init__(self, payments_config, app):
+    def __init__(self, payments_config):
         self.api_key = payments_config["nmi"]["api_key"]
         self.username = payments_config["nmi"]["username"]
-        self.username = payments_config["nmi"]["password"]
-        
-        app.add_url_rule(
-            "/plugins/payments/nmi_callback",
-            view_func=self.step_two_callback,
-            methods=["POST", "GET"]
-        )
-        self.redirect_url = "http://localhost:5000/plugins/payments/nmi_callback"
-        self.tokens = {}
+        self.password = payments_config["nmi"]["password"]
 
+    # Converts a Python dict to an XML formatted string
     def dict2xml(self, structure, tostring=True):
         def dict2element(root, structure, doc):
             if isinstance(root, str):
@@ -42,6 +32,7 @@ class Payment():
         dict2element(doc.documentElement, value, doc)
         return doc.toxml() if tostring else doc
 
+    # Takes a Python dict and sends it to the API as XML
     def send_dict(self, data):
         # convert dict to XML
         xml = self.dict2xml(data)
@@ -52,6 +43,7 @@ class Payment():
             headers=headers
         ).text
 
+    # Gets the value of the first element: `element` from XML string
     def get_xml_value(self, element, xml):
         value = minidom.parseString(xml)\
             .getElementsByTagName(element)[0]\
@@ -59,48 +51,84 @@ class Payment():
             .nodeValue
         return value
 
-    def step_one(self):
+    # Requests a form URL for stage 1 of NMI's 'three-step' API
+    def get_form_url(self, redirect_url):
         data = {
             "add-customer": {
-                "redirect-url": self.redirect_url,
+                "redirect-url": redirect_url,
                 "api-key": self.api_key
             }
         }
-        print(data)
-        res = requests.post(
-            "https://secure.nmi.com/api/v2/three-step",
-            self.dict2xml(data),
-            headers={"Content-Type": "text/xml"}
-        )
-        return self.get_xml_value('form-url', res.text)
+        res = self.send_dict(data)
+        return self.get_xml_value('form-url', res)
 
-
-    def step_two_callback(self):
+    # Can be called from within a flask route to respond as the redirect page
+    def form_url_callback(self, request):
+        from flask import request
         token_id = request.args.get("token-id")
-        res = self.step_three(token_id)
+        print (token_id)
+        res = self.complete_action(token_id)
         if res["response"]["result"] == "1":
-            return self.do_transaction(1, res["response"]["customer-vault-id"])
-        return "some Error"
+            return res
+        return "Credit card info was rejected"
 
-    def step_three(self, token_id):
+    # Sends the final request to complete the 'three-step' "transaction"
+    def complete_action(self, token_id):
         res = self.send_dict({
             "complete-action": {
-                "api-key": '2F822Rw39fx762MaV7Yy86jXGTC7sCDy',
+                "api-key": self.api_key,
                 "token-id": token_id
             }
         })
         data = xmltodict.parse(res)
-
         return data
 
+    # Emulates the browser's form request on the back end
+    def send_cc(self, cc, exp, form_url, cvv=False):
+        form_data = {
+            'billing-cc-number': cc,
+            'billing-cc-exp': exp
+        }
+        if cvv:
+            # cvv is optional
+            form_data['billing-cvv'] = cvv
+        # make request but don't use the redirect
+        response = requests.post(form_url, form_data, allow_redirects=False)
+        return True if response.status_code == 301 else Exception()
+
+    # Handles the whole 'three-step' process in one step.
+    def create_vault(self, cc, exp, cvv=False):
+        # get a form url
+        # set the redirect url as something neutral because we're not using it
+        form_url = self.get_form_url('https://secure.nmi.com/api/v2/three-step')
+        # parse the token from the request URL
+        token_id = form_url[41:]
+        # send the cc deets. we don't need the return value
+        self.send_cc(cc, exp, form_url, cvv=cvv)
+        completed = self.complete_action(token_id)
+        return completed['response']
+
+    # Charges a vault ID
     def do_transaction(self, amount, vault):
-        print("%.2f" % amount)
         return requests.post(
             "https://secure.networkmerchants.com/api/transact.php",
             {
                 "customer_vault_id": vault,
-                "username": "demo",
-                "password": "password",
+                "username": self.username,
+                "password": self.password,
                 "amount": "%.2f" % amount
             }
         ).text
+
+if __name__ == "__main__":
+    # Testing info
+    payments_config = {
+        "nmi":{
+            "api_key": "2F822Rw39fx762MaV7Yy86jXGTC7sCDy",
+            "username": "demo",
+            "password": "password"
+        }
+    }
+    p = Payment(payments_config)
+    # sample info
+    print(p.create_vault('4111111111111111', '10/25', cvv='999'))
