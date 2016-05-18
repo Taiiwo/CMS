6b292078f2aabda53776339adc5edce45d8a87e0
Emati Mitame <GitCommits@mita.me>
2016-05-18 23:32:50 +0100

    Fix main.py because I was an idiot

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from taiicms import app, socket, config
 
 
 def main():
-    socketio.run(app, config["bind_addr"], config["port"], debug=config["debug"])
+    socket.run(app, config["bind_addr"], config["port"], debug=config["debug"])
 
 if __name__ == "__main__":
     main()
