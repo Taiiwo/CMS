4ff7eb5e7169d641b4a641aa0d4b200b012ee2c9
Taiiwo Llort <taiiwo@llort.gq>
2016-05-18 20:21:08 +0100

    Complete add-card route

--- plugins/payment/__init__.py
+++ plugins/payment/__init__.py
@@ -7,24 +7,31 @@ from xml.dom import minidom
 from flask import request
 import requests
 import json
-
-
-payments_config = None
-pm = None
-
+import re
 
 def main(config):
     global payments_config, pm
     payments_config = config
-    pm = Payment(payments_config, app)
+    pm = False
 
-
-@app.route("/api/plugin/payment/step_one", methods=["POST"])
-def request_money():
-    user_data = user.authenticate()
-    print(user_data)
-    if user_data is None and not app.debug:
+@app.route("/api/plugin/payment/add-card", methods=["POST"])
+def add_card():
+    usern = user.authenticate()
+    if not usern:
         return make_error_response("login_required")
-    return make_success_response({
-        "url": pm.step_one()
-    })
+    try:
+        cc = request.form['cc']
+        exp = request.form['exp']
+        cvv = request.form['cvv'] if 'cvv' in request.form else False
+    except KeyError as e:
+        return make_error_response('data_required', e.args)
+    global pm
+    if not pm:
+        pm = Payment(payments_config)
+    vault = pm.create_vault(cc, exp, cvv=cvv)
+    if vault['result'] == "1":
+        # store the vault info somewhere in the user data
+
+        return make_success_response()
+    else:
+        return make_error_response('data_invalid')
