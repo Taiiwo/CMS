584b13d5e9eea28008a5597fe04ae3e8a6e3c3a0
Taiiwo Llort <taiiwo@llort.gq>
2016-09-07 09:18:31 +0100

    implement make_payment and general fixes

--- plugins/payment/__init__.py
+++ plugins/payment/__init__.py
@@ -5,18 +5,20 @@ from taiicms.api import make_error_response, make_success_response, user, util
 from collections import Mapping
 from xml.dom import minidom
 from flask import request
+from bson import ObjectId
 import requests
 import time
 import json
 import re
 
 def main(config):
-    global payments_config, pm, users
+    global payments_config, pm
     payments_config = config
-    pm = False
-    users = util.get_collection('users', db=util.config["auth_db"])
-    payment_user = users.find_one({"username": "Payments"}, {"_id": True})
-    payment_user = users.find_one({"username": "Products"}, {"_id": True})
+    pm = Payment(payments_config)
+
+users = util.get_collection('users', db=util.config["auth_db"])
+payments_user = users.find_one({"username": "Payments"})
+products_user = users.find_one({"username": "Products"})
 
 @app.route("/api/plugin/payment/add-card", methods=["POST"])
 def add_card():
@@ -31,8 +33,6 @@ def add_card():
     except KeyError as e:
         return make_error_response('data_required', e.args)
     global pm
-    if not pm:
-        pm = Payment(payments_config)
     vault_unrefined = pm.create_vault(cc, exp, cvv=cvv)
     if vault_unrefined['result'] == '3':
       return make_error_response("data_invalid", vault_unrefined['result-text'])
@@ -45,11 +45,12 @@ def add_card():
     }
     if vault_unrefined['result'] == "1":
         # store the vault info somewhere in the user data
-        usern = users.find_and_modify({"_id": usern["_id"]}, {
+        users.update_one({"_id": usern["_id"]}, {
             "$push": {
                 "nmi_vaults": vault
             }
         })
+        usern = users.find_one({"_id": usern['_id']})
         return make_success_response({"vaults": usern["nmi_vaults"]})
     else:
         return make_error_response('data_invalid')
@@ -118,6 +119,8 @@ def get_payment_methods():
     usern = user.authenticate()
     if not usern:
         return make_error_response("login_required")
+    if not "nmi_vaults" in usern:
+        return make_error_response("data_missing", "user has no vaults")
     if 'default_method' in usern:
         default_card = usern['default_method']
     else:
@@ -136,8 +139,9 @@ def get_payment_methods():
 
 def get_product(product_id):
     products = util.get_collection('products')
-    product_id = ObjectId(product_id) if type(product_is) is str else product_id
-    return products.find_one({'_id': product_id, sender: products_user['_id']})
+    product_id = ObjectId(product_id) if type(product_id) is str else product_id
+    print(products_user['_id'])
+    return products.find_one({'_id': ObjectId(product_id), "sender": str(products_user['_id'])})
 
 # Posts to a datachest, but accepts money instead of a session key
 @app.route("/api/plugin/payment/make-payment", methods=['POST'])
@@ -146,43 +150,44 @@ def make_payment():
     if not usern:
         return make_error_response("login_required")
     try:
-        # recipient is the customer
-        recipient = request.form['user_id']
-        # id of the requested product
-        product_id = request.form['product_id']
-        payment_index = request.form['payment_index']
-        shipping = request.form['shipping'] or False
-        prepaid = request.form['recur_prepaid'] or False
-        recur_at_day = request.form['recur_at_day'] or False
+        orders = json.loads(request.form['orders'])
+        shipping = json.loads(request.form['shipping'])
+        payment_method = request.form['payment_method']
     except KeyError as e:
         return make_error_response('data_required', e.args)
-    product = get_product(product_id)
-    if not product: return make_error_response('invalid_data', 'product_id')
-    product = product['data']
-    if product['require_shipping'] and not shipping:
-        return make_error_response('data_required', 'shipping')
-    vault = usern['nmi_vaults'][payment_index]
-    # do the transaction for both recuring and non-recuring payments
-    if prepaid_days:
-        amount = product['amount'] * prepaid_days
-    transaction = pm.do_transaction(amount, vault['vault_id'])
+    # build order list
+    order_list = []
+    cost = 0
+    for order in orders:
+        print(order)
+        order_item = {}
+        product = get_product(order['id'])
+        print(product)
+        if not product:
+            return make_error_response('data_invalid', 'product_id')
+        if product['data']['require_shipping']:
+            if not shipping:
+                return make_error_response('data_required', 'shipping')
+            order_item['shipping'] = shipping
+        order_item['product'] = product
+        order_item['quantity'] = order['quantity']
+        order_item['amount'] = order_item['product']['data']['amount']\
+                * order_item['quantity']
+        cost += order_item['amount']
+        order_list.append(order_item)
+    
+    # charge the user the cost
+    transaction = pm.do_transaction(cost, usern['nmi_vaults'][int(payment_method)])
     if not transaction:
         return make_error_response("data_invalid", "transaction-failed")
-    sale = {}
-    sale['recipient'] = recipient
-    sale['sender'] = payment_user['_id']
-    sale['ts'] = time.time()
-    sale['data'] = {}
-    # day of month of payment
-    sale['data']['recur_at_day'] = recur_at_day
-    # number of prepaid days/months based on recur_type
-    sale['data']['recur_prepaid'] = prepaid
-    # product reference
-    sale['data']['product_id'] = product_id
-    # shipping data
-    sale['data']['shipping'] = shipping
-    sale['data']['']
-    util.store(sale, 'sales', visible=True)
+    for order in order_list:
+        sale = {}
+        sale['recipient'] = str(usern['_id'])
+        sale['sender'] = str(payments_user['_id'])
+        sale['ts'] = time.time()
+        sale['data'] = order
+        util.store(sale, 'sales', visible=True)
+    return make_success_response()
 
 """
 example product
